f35c4595be71c7c1d52dbb79991ebbd5df82aec4
Sudiksha Sharma <sush02@dfki.de>
2025-01-19 20:51:35 +0100

    Added and linked SFML lib

--- scripts/src/main.cpp
+++ scripts/src/main.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
+#include<SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout<<"HEY";
+	sf::RenderWindow myWindow{ sf::VideoMode(400, 400), "MyWindow" };
+	while (myWindow.isOpen())
+	{
+		sf::Event event;
+		while (myWindow.pollEvent(event))
+		{
+			if (event.type == sf::Event::EventType::Closed) {
+				myWindow.close();
+			}
+		}
+
+	}
 	return 0;
 }
